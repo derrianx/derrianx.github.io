f11faea07787cc8601945956cc3b609771d7b286
derrianx <121491871+derrianx@users.noreply.github.com>
2023-03-18 02:12:45 +0000

    daily push

---
+++ python/coin-flask.py
@@ -0,0 +1,4 @@
+# write a flask app that does coin.py
+
+from flask import Flask, render_template, request
+import random
---
+++ python/coin.py
@@ -0,0 +1,31 @@
+# Description: Coin flip simulator
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# generate a array of coin flip results
+array = np.array(['H', 'T', 'T', 'H', 'T', 'H', 'T','H','T','H', 'T','T','T','T','T','T','T','T','T','T'])
+
+# write a python function that uses bayes theorem to estimate the probability of hettings heads or tails
+def bayes(array):
+    # count the number of heads and tails in the array
+    heads = array.count('H')
+    tails = array.count('T')
+    # calculate the probability of heads and tails
+    prob_heads = heads / len(array)
+    prob_tails = tails / len(array)
+    # calculate the probability of heads given tails and tails given heads
+    prob_heads_given_tails = prob_heads / prob_tails
+    prob_tails_given_heads = prob_tails / prob_heads
+    # return the probability of heads and tails
+    return prob_heads, prob_tails, prob_heads_given_tails, prob_tails_given_heads
+
+# print the probability of heads and tails
+
+# print(bayes(array))
+
+# use a seaborn countplot to visualize the results
+sns.distplot(array, stat="density")
+# display the plot
+
+plt.savefig('my_plot.png')
